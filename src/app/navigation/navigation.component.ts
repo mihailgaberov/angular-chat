05fc9a11c20af22a1679192e2702638907c18d46
mihailgaberov <$Vale@meama010>
2020-08-11 23:43:24 +0300

    :sparkles: Styled Navigation component.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faComment, faCog } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navigation',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+  faComment = faComment;
+  faCog = faCog;
 
   constructor() { }
 
