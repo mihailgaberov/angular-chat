80cd30acf3c1731213ebfd7a59f8930b989572ab
mihailgaberov <mihail.gaberov@gmail.com>
2020-08-18 23:06:24 +0300

    :sparkles: Integrated user-profile.component with RxJs observables in order to update translations runtime.

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {TranslationsService} from '../translations/translations.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-user-profile',
@@ -8,12 +9,16 @@ import {TranslationsService} from '../translations/translations.service';
 })
 export class UserProfileComponent implements OnInit {
   public usernameLabel: string;
+  private translationSubscription: Subscription;
 
   constructor(private translationService: TranslationsService) {
     this.usernameLabel = translationService.getActiveTranslation().properties.usernameLabel;
   }
 
   ngOnInit(): void {
+    this.translationSubscription = this.translationService.subscribe((data) => {
+      this.usernameLabel = data.properties.usernameLabel;
+    });
   }
 
 }
