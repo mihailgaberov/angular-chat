a53fa64139d4c83010e588a784c2fd54e6d5b086
mihailgaberov <mihail.gaberov@gmail.com>
2020-08-24 23:00:23 +0300

    :sparkles: Extracted theme in an enum. Set the correct theme radio button selected. Installed lodash.debounce.

--- src/app/theme-selector/theme-selector.component.ts
+++ src/app/theme-selector/theme-selector.component.ts
@@ -1,26 +1,30 @@
-import {Component, OnInit} from '@angular/core';
-import {ThemeService} from 'src/app/theme/theme.service';
-import {TranslationsService} from '../translations/translations.service';
-import {Subscription} from 'rxjs';
-import {readRecord, storeToLocalStorage} from '../utilities/localStorageService';
-import {dark, light} from '../theme/theme';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/theme/theme.service';
+import { TranslationsService } from '../translations/translations.service';
+import { Subscription } from 'rxjs';
+import { readRecord, storeToLocalStorage } from '../utilities/localStorageService';
+import { dark, light } from '../theme/theme';
+import { THEMES } from '../enums/themes';
 
 @Component({
   selector: 'app-theme-selector',
   templateUrl: './theme-selector.component.html',
   styleUrls: ['./theme-selector.component.scss']
 })
+
 export class ThemeSelectorComponent implements OnInit {
   public interfaceColor: string;
   public lightColorLabel: string;
   public darkColorLabel: string;
   private translationSubscription: Subscription;
+  public selectedTheme: boolean;
 
-
-  constructor(private themeService: ThemeService, private translationService: TranslationsService) {}
+  constructor(private themeService: ThemeService, private translationService: TranslationsService) {
+    this.selectedTheme = readRecord('theme') === THEMES.light;
+  }
 
   ngOnInit(): void {
-    this.themeService.setActiveTheme(readRecord('theme') === 'light' ? light : dark);
+    this.themeService.setActiveTheme(readRecord('theme') === THEMES.light ? light : dark);
     this.translationSubscription = this.translationService.subscribe((data) => {
       this.interfaceColor = data.properties.interfaceColor;
       this.lightColorLabel = data.properties.colors.color1;
@@ -30,11 +34,11 @@ export class ThemeSelectorComponent implements OnInit {
 
   setLightTheme(): void {
     this.themeService.setLightTheme();
-    storeToLocalStorage('theme', 'light');
+    storeToLocalStorage('theme', THEMES.light);
   }
 
   setDarkTheme(): void {
     this.themeService.setDarkTheme();
-    storeToLocalStorage('theme', 'dark');
+    storeToLocalStorage('theme', THEMES.dark);
   }
 }
